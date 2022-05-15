82c4bd7a65cbc2fd0e63926dc296c8f363e8b696
Stephan <stephan.folioo@gmail.com>
2022-05-15 20:57:14 +0200

    Mise en place de 'Authentification sans mdp' & protéger les pages

--- src/App.js
+++ src/App.js
@@ -1,67 +1,48 @@
 import Header from "./components/Header/Header";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
 import Profile from "./pages/Profile/Profile";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Auth from "./guards/Auth";
 
 function App() {
   const [user, setUser] = useState(null);
+
   return (
     <>
-      <Header logout={() => setUser(false)} />
+      <Header user={user} logout={() => setUser(false)} />
       <Routes>
         <Route path="/" element={<Home PageName="Home Page" />} />
+        {!user && (
+          <>
+            <Route
+              path="/login"
+              element={
+                <Login
+                  PageName="Login Page"
+                  authenticate={() => setUser(true)}
+                />
+              }
+            />
+          </>
+        )}
+        {user && (
+          <Route
+            path="/profile"
+            element={
+              <Profile logout={() => setUser(false)} PageName="Profile Page" />
+            }
+          />
+        )}
 
-        <Route
-          path="/login"
-          element={
-            <Login PageName="Login Page" authenticate={() => setUser(true)} />
-          }
-        />
-
-        <Route
-          path="/profile"
-          element={
-            <Profile logout={() => setUser(false)} PageName="Profile Page" />
-          }
-        />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
       <Footer />
     </>
   );
-  // <div className="App">
-  //   <Router>
-  //     <Header logout={() => setUser(false)} />
-  //     <Routes>
-  //       {!user && (
-  //         <>
-  //           <Route path="/" element={<Home PageName="Home Page" />} />
-
-  //           <Route path="/login" element={<Login PageName="Login Page" />} />
-  //         </>
-  //       )}
-
-  //       {!user && (
-  //         <Route
-  //           path="/profile"
-  //           element={
-  //             <Profile
-  //               logout={() => setUser(false)}
-  //               PageName="Profile Page"
-  //             />
-  //           }
-  //         />
-  //       )}
-
-  //       <Route path="*" element={<ErrorPage />} />
-  //     </Routes>
-  //     <Footer />
-  //   </Router>
-  // </div>
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Header({ logout }) {
+function Header({ user, logout }) {
   return (
     <nav className="main-nav">
+      {console.log(user)}
       <Link className="main-nav-logo" to="/">
         <img
           className="main-nav-logo-image"
@@ -13,18 +14,23 @@ function Header({ logout }) {
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
       <div>
-        <Link className="main-nav-item" to="/login">
-          <i className="fa fa-user-circle"></i>
-          Sign In
-        </Link>
-        <Link className="main-nav-item" to="./user.html">
-          <i className="fa fa-user-circle"></i>
-          Tony
-        </Link>
-        <Link onClick={logout} className="main-nav-item" to="/">
-          <i className="fa fa-sign-out"></i>
-          Sign Out
-        </Link>
+        {!user ? (
+          <Link className="main-nav-item" to="/login">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </Link>
+        ) : (
+          <>
+            <Link className="main-nav-item" to="./user.html">
+              <i className="fa fa-user-circle"></i>
+              Tony
+            </Link>
+            <Link onClick={logout} className="main-nav-item" to="/">
+              <i className="fa fa-sign-out"></i>
+              Sign Out
+            </Link>
+          </>
+        )}
       </div>
     </nav>
   );
