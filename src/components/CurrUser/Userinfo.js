ef9a5d9db2d370b74b920281ed67003c4d16051b
Stephan <stephan.folioo@gmail.com>
2022-05-18 11:45:21 +0200

    ajout d'un formulaire pour modifier les informations de l'utilisateur sans le css

--- src/components/CurrUser/Userinfo.js
+++ src/components/CurrUser/Userinfo.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
 const Userinfo = () => {
+  const [displayForm, setDisplayForm] = useState(false);
+
   const userData = useSelector((state) => state.userInfos.userInfos);
 
-  console.log(userData);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  // console.log(userData);
   return (
     <div className="header">
       <h1>
@@ -12,7 +18,28 @@ const Userinfo = () => {
         <br />
         {userData?.firstName} {userData?.lastName}!
       </h1>
-      <button className="edit-button">Edit Name</button>
+      {displayForm ? (
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div>
+            <label htmlFor="firstname">First Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="lastname">Last Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <button>Save</button>
+            <button type="button" onClick={() => setDisplayForm(false)}>
+              Cancel
+            </button>
+          </div>
+        </form>
+      ) : (
+        <button className="edit-button" onClick={() => setDisplayForm(true)}>
+          Edit Name
+        </button>
+      )}
     </div>
   );
 };
